18e959a283a16a0964087107c676fb095450725e
Beyou-epsi <corentin.beyries@ecoles-epsi.net>
2024-10-09 09:12:27 +0200

    [ADD] Company / Certificat Models and relationship

--- models/Equipment.js
+++ models/Equipment.js
@@ -1,42 +1,50 @@
-const { Model, DataTypes } = require('sequelize');
+const {Model, DataTypes} = require('sequelize');
 const sequelize = require('../sequelize');
+const Company = require('../models/Company');
 
-class Equipment extends Model {}
+class Equipment extends Model {
+}
 
 Equipment.init(
     {
-      id: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        defaultValue: DataTypes.UUIDV4,
-      },
-      type: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      model: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      dateInstallation: {
-        type: DataTypes.DATEONLY,
-        allowNull: false,
-      },
-      dateVerification: {
-        type: DataTypes.DATEONLY,
-        allowNull: false,
-      },
+        id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV4,
+        },
+        company_id: {
+            type: DataTypes.UUID,
+            allowNull: false,
+        },
+        type: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        model: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        dateInstallation: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+        },
+        dateVerification: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+        },
     },
     {
-      sequelize,
-      modelName: 'Equipment',
-      tableName: 'equipment',
-      timestamps: false,
+        sequelize,
+        modelName: 'Equipment',
+        tableName: 'equipment',
+        timestamps: false,
     }
 );
+
+Equipment.belongsTo(Company, {foreignKey: 'company_id'});
 
 module.exports = Equipment;
